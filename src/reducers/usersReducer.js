1341435efcba4b17e38ca1bf35e20bc86ce60509
Cobboldgit <86422221+Cobboldgit@users.noreply.github.com>
2021-10-06 11:12:11 +0000

    create initialState and set userReducer

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  users: [
+    {
+      name: "Augustine",
+      email: "augustine@gmail.com",
+      gen: 19,
+    },
+    {
+      name: "Imma",
+      email: "imma@gmail.com",
+      gen: 19,
+    },
+    {
+      name: "Gab",
+      email: "gab@gmail.com",
+      gen: 19,
+    },
+  ],
+};
+
+const usersReducer = (state = initialState, action) => {
+  return state;
+};
