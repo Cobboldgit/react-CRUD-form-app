8c34760061af3ea696d50c8ff38372bea285f7c2
Cobboldgit <86422221+Cobboldgit@users.noreply.github.com>
2021-10-06 10:57:13 +0000

    install redux and react-redux

--- src/component/User.js
+++ src/component/User.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 function User(props) {
-    const user=props.user;
+  const user = props.user;
   return (
     <div>
       <h1>{user.name}</h1>
       <h3>{user.email}</h3>
       <h3>{user.gen}</h3>
-      <Button variant="primary">
-        Edit
-      </Button>
+      <Button variant="primary">Edit</Button>
       <Button variant="danger" onClick={() => props.delectUser(user.id)}>
         Delete
       </Button>
