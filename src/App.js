30358058e351f257ed1ea8b105726daf11838144
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-10-20 11:29:42 +0000

    remove delectUser and editUser functions and also remove their props being pass to UserList component

--- src/App.js
+++ src/App.js
@@ -8,39 +8,12 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
 
-  // function AddUser(user) {
-  //   setUsers([...users, user]);
-  // }
-
-  //function to delete a user
-  function delectUser(userId) {
-    //loops through the users state and removes the user with the same id
-    const filteredList = users.filter((user) => {
-      return userId !== user.id;
-    });
-
-    //sets the users state to the filtered users array
-    setUsers(filteredList);
-  }
-
-  const editUser = (newUser, userId) => {
-    setUsers(
-      users.map((user) => {
-        if (userId === user.id) {
-          return newUser;
-        } else {
-          return user;
-        }
-      })
-    );
-  };
-
   return (
     <>
       <div className="App">
         <div className="container">
-          <UserInput/>
-          <UserList delectUser={delectUser} editUser={editUser} />
+          <UserInput />
+          <UserList />
         </div>
       </div>
     </>
