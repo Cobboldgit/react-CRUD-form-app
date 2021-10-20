c6a8be946a97bb97a4c14e0acd8a7c6e060359cb
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-10-20 11:36:42 +0000

    create reducer for deleteUser and editUser inside usersReducer.js

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -5,7 +5,20 @@ const initialState = {
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_USER":
-      return {...state, users: [...state.users, action.payload]}
+      return { ...state, users: [...state.users, action.payload] };
+    case "DELETE_USER":
+      const filterUser = state.users.filter((user) => {
+        return user.id !== action.payload;
+      });
+      return { ...state, users: filterUser };
+    case "EDIT_USER":
+      const updateUser = state.users.map((user) => {
+        if (user.id === action.payload.userId) {
+          return action.payload.updateUser
+        }
+        return user
+      })
+      return {...state, users: updateUser}
     default:
       return state;
   }
