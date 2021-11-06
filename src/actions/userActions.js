7bc956edab500ab39f84249d33139b9ab6b4ddec
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-11-06 12:07:22 +0000

    create getAllUsers action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,20 +1,45 @@
 export const addUser = (user) => {
-  return {
-    type: "ADD_USER",
-    payload: user,
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .add(user)
+      .then(
+        () => {},
+        () => {}
+      );
   };
+  // return {
+  //   type: "ADD_USER",
+  //   payload: user,
+  // };
 };
 
 export const deleteUser = (userId) => {
-  return {
-    type: "DELETE_USER",
-    payload: userId,
-  };
+  return (dispatch, state, { getFirestore }) => {};
+  // return {
+  //   type: "DELETE_USER",
+  //   payload: userId,
+  // };
 };
 
 export const editUser = (userId, updateUser) => {
   return {
     type: "EDIT_USER",
     payload: { userId, updateUser },
+  };
+};
+
+export const getAllUsers = () => {
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .onSnapshot((querySnapshot) => {
+        let users = [];
+        querySnapshot.forEach((doc) => users.push(doc.data()));
+        dispatch({
+          type: "GET_ALL_USERS",
+          payload: users,
+        });
+      });
   };
 };
